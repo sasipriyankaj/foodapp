98f646b92940f92e84f883af2c54e1e84bab5468
Joy Dey <joydey.bd.2002@gmail.com>
2023-03-08 13:33:49 +0600

    data updated

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,6 +1,5 @@
 import "./Cart.css";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import SectionTop from "../../components/SectionTop/SectionTop";
 
 const Cart = () => {
